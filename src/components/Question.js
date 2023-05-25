684404b4dba3b878f729420fa3ed53cca4a308b9
Eric Nelson <ernelson12o0@gmail.com>
2023-05-25 15:56:07 -0400

    Lab Complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining(timeRemaining-1)
+    }, 1000);
+    return function cleanup() {
+      clearTimeout(timer)
+    }
+  },
+  [timeRemaining]);
+  function resetTimeout() {
+    timeRemaining === 0 ? (setTimeRemaining(10), onAnswered(false)) : null;
+  }
+  useEffect(() => {
+    resetTimeout()
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
